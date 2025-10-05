610d8b43567cf52415c80e133e7977373621e301
HMODY <185634831+HMODY849@users.noreply.github.com.>
2025-10-05 06:05:51 -0700

    finish set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+     mode: "development",
+  entry: './src/index.js', 
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
   },
-};
+  devServer:{
+        port: 8080,
+        open: true,
+},
+plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/templet.html',
+    }),
+  ],
+}
+// module.exports ={
+//     devServer:{
+//         port: 8080,
+//         open: true,
+//     }
+// }
